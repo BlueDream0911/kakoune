BlueDream0911/kakoune

commit aa6f7c82e9dc460c6f12a30bae850479698906a7

Fix some posix compliance in man.kak

rc/man.kak
@@ -38,7 +38,7 @@ def -params .. \
     prefix=${1:0:${kak_pos_in_token}}
     for page in /usr/share/man/*/${prefix}*.[1-8]*; do
         candidate=$(basename ${page%%.[1-8]*})
-        pagenum=$(sed -r 's,^.+/.+\.([1-8][^.]*)\..+$,\1,' <<< $page)
+        pagenum=$(echo $page | sed 's/^.*\([1-8][a-z]*\)$/\1/')
         case $candidate in
             *\*) ;;
             *) echo $candidate\($pagenum\);;

commit 858013f33b023409fee90bbbfc19ed52a2c30c72

Fix a regression introduced in aa6f7c82.
The regular expression changed, and returned the wrong page number to
the command completion.

rc/man.kak
@@ -38,7 +38,7 @@ def -params .. \
     prefix=${1:0:${kak_pos_in_token}}
     for page in /usr/share/man/*/${prefix}*.[1-8]*; do
         candidate=$(basename ${page%%.[1-8]*})
-        pagenum=$(echo $page | sed 's/^.*\([1-8][a-z]*\)$/\1/')
+        pagenum=$(echo $page | sed -r 's,^.+/.+\.([1-8][^.]*)\..+$,\1,')
         case $candidate in
             *\*) ;;
             *) echo $candidate\($pagenum\);;